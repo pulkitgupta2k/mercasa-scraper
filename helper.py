80fb3a28e3a34b6865020082cec5e26cf0ac4073
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-11 03:33:22 +0530

    basic functionality updated

--- helper.py
+++ helper.py
@@ -27,12 +27,10 @@ def getProducts():
         headings.append(heading.text.strip())
 
     for s in soup.findAll("div",{"class" : "formProductos"}):
-        product = []
+        product = {}
         for item in s.findAll("div",{"class": "formProducto"}):
-            p = {}
             i = item.find("input")
-            p[i['id'].replace("checkbox-productos-", "")] = item.text.strip()
-            product.append(p)
+            product[i['id'].replace("checkbox-productos-", "")] = item.text.strip()
         products.append(product)
 
     products_dict = {}
@@ -53,7 +51,7 @@ def getData(product_no):
     date_end = "2020-05-10"
     productos = product_no
     headers = {'Host' : 'www.mercasa.es', 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0', 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'Accept-Language' : 'en-US,en;q=0.5', 'Accept-Encoding' : 'gzip, deflate', 'Content-Type' : 'application/x-www-form-urlencoded', 'Connection' : 'close', 'Referer' : 'https://www.mercasa.es/red-de-mercas/precios-y-mercados-mayoristas/grafica', 'Upgrade-Insecure-Requests' : '1', 'DNT' : '1'}
-    payload = "mayoristas=1%2C2%2C3%2C4%2C5&productos={}%2C147&media=on&fechaInicio={}&fechaFin={}".format(productos, date_start, date_end)
+    payload = "mayoristas=1%2C2%2C3%2C4%2C5&productos={}&media=on&fechaInicio={}&fechaFin={}".format(productos, date_start, date_end)
     # print(payload)
     req = requests.post(link, data=payload, headers = headers)
     html = req.content
@@ -61,9 +59,22 @@ def getData(product_no):
     # print(soup)
     chart = soup.find("div",{"class" : "mychart dentroSeccion"})
     script = chart.find("script").text.strip()[16:-2]
+    # print(script)
     script = find_between(script, "data: [", "]").strip()
     script = script.replace("fecha", "\"fecha\"")
     script = eval(script)
-    pprint(script)
+    print(script)
+    return script
+
+
+def driver():
+    with open("products.json") as f:
+        products_json = json.load(f)
+    # print(products)
+    for heading,products in products_json.items():
+        for product_no, product_name in products.items():
+            print(getData(product_no))
+            print(product_name)
+
 
 # getData(46)
